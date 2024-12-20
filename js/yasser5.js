b500fb58bd0e46993a6863339f56d841995f0435
yasseralasser <engyasseralasser81@gmail.com>
2024-12-20 18:55:14 +0200

    first commit

---
+++ js/yasser5.js
@@ -0,0 +1,15 @@
+// $(document).ready(function(){
+//     $('.carousel').carsouel({
+//         interval:5000
+//     });
+// });
+
+// let element = document.querySelector('.media-opject');
+// element.classList.add('animate__animated', 'animate__bounceOutLeft');
+
+// $(function() {
+// 	$("html").niceScroll({
+//        cursorcolor: "#F00" 
+//        //Change the scroll bar color
+//      });
+// });
